7f89e6bec5e3d751d4789c93d1586ada94c6f2e5
Yves Chevallier <nowox@x0x.ch>
2018-05-14 22:58:21 +0200

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <b-nav tabs>
+      <b-nav-item :to="{ path: 'login'}">Login</b-nav-item>
+      <b-nav-item :to="{ path: 'counter'}">Counter</b-nav-item>
+    </b-nav>
+    <div class="hello">
+    <img src="./assets/logo.png">
+    <h1>{{ $route.name }}</h1>
+    <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hello">
+    <b-container fluid>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group id="exampleInputGroup2"
+                    label="Login:"
+                    label-for="login">
+        <b-form-input id="login"
+                      type="text"
+                      v-model="form.login"
+                      required
+                      placeholder="Login">
+        </b-form-input>
+        <b-form-input id="password"
+                      type="password"
+                      v-model="form.password"
+                      required
+                      placeholder="Password">
+        </b-form-input>    
+        </b-form-group>
+      <b-button type="submit" variant="primary">Login</b-button>
+    </b-form>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      form: {
+        email: '',
+        name: '',
+        food: null,
+        checked: []
+      },
+      foods: [
+        { text: 'Select One', value: null },
+        'Carrots', 'Beans', 'Tomatoes', 'Corn'
+      ],
+      show: true
+    }
+  },
+  methods: {
+    onSubmit (evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset (evt) {
+      evt.preventDefault();
+      /* Reset our form values */
+      this.form.email = '';
+      this.form.name = '';
+      this.form.food = null;
+      this.form.checked = [];
+      /* Trick to reset/clear native browser form validation state */
+      this.show = false;
+      this.$nextTick(() => { this.show = true });
+    }
+  }
+}
+</script>
